e0c88b36afbb67f5d805cff07769f14c918d9033
stayFoolish <nemanjakaraklajic90@icloud.com>
2024-11-06 11:49:02 +0100

    refactor: Add PNG module declarations and update App component
    Added PNG module declarations for TypeScript in both section-10 and section-11. Updated App component in section-11 to include various imports, state hooks, and new functionalities like place selection and deletion modal management. Made "fallbackText" prop optional in Places component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,74 @@
+import { useRef, useState } from "react";
+import logoImg from "./assets/logo.png";
+import { AVAILABLE_PLACES } from "./data.ts";
+import Modal from "./components/Modal.tsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.tsx";
+import Places from "./components/Places.tsx";
+import { ModalRef, PlaceType } from "./types";
+
 function App() {
-  return <div>App</div>;
+  const modalRef = useRef<ModalRef>(null);
+  const selectedPlaceRef = useRef<string>(null);
+  const [pickedPlaces, setPickedPlaces] = useState<PlaceType[]>([]);
+
+  function handleStartRemovePlace(id: string) {
+    modalRef?.current?.open();
+    selectedPlaceRef.current = id;
+  }
+
+  function handleStopRemovePlace() {
+    modalRef?.current?.close();
+  }
+
+  function handleSelectPlace(id: string) {
+    setPickedPlaces((prevPickedPlaces) => {
+      if (prevPickedPlaces.some((place) => place.id === id)) {
+        return prevPickedPlaces;
+      }
+      const place = AVAILABLE_PLACES.find((place) => place.id === id);
+      return [place, ...prevPickedPlaces];
+    });
+  }
+
+  function handleRemovePlace() {
+    setPickedPlaces((prevPickedPlaces) =>
+      prevPickedPlaces.filter((place) => place.id !== selectedPlaceRef.current),
+    );
+    modalRef?.current?.close();
+  }
+
+  return (
+    <>
+      <Modal ref={modalRef}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt="Stylized globe" />
+        <h1>PlacePicker</h1>
+        <p>
+          Create your personal collection of places you would like to visit or
+          you have visited.
+        </p>
+      </header>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText={"Select the places you would like to visit below."}
+          places={pickedPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+        <Places
+          title="Available Places"
+          places={AVAILABLE_PLACES}
+          onSelectPlace={handleSelectPlace}
+        />
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -3,3 +3,8 @@ declare module "*.jpg" {
   const value: string;
   export default value;
 }
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
